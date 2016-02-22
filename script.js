7750b8088ce7e4c4138f2958fe8a658810b3c733
yguff88 <alec.mcguffey@gmail.com>
2016-02-21 23:39:21 -0500

    wrote js 1st draft

---
+++ script.js
@@ -0,0 +1,32 @@
+// set up variables
+var cityChoice;
+/* 
+1. On clicking submit, save the value of the text from the city-type inpt form
+2. Automatically remove any existing 'body' classes so you don't have multiple city backgrounds
+3. If the user submitted value equals any of the city names, change background to that city
+
+*/
+function submit(){
+	$('body').removeClass();
+	cityChoice = $('#city-type').val().toLowerCase();
+	if(cityChoice==="new york" || cityChoice==="new york city" || cityChoice==="nyc") {
+		$('body').addClass("nyc");
+	} else if (cityChoice==="Austin" || cityChoice==="atx") {
+		$('body').addClass("austin");
+	} else if (cityChoice==="san francisco" || cityChoice==="sf" || cityChoice==="bay area") {
+		$('body').addClass("sf");
+	} else if (cityChoice==="los angeles" || cityChoice==="la" || cityChoice==="lax") {
+		$('body').addClass("la");
+	} else if (cityChoice==="sydney" || cityChoice==="syd") {
+		$('body').addClass("sydney");
+	} else {
+		$('body').addClass("other");
+	}
+};
+	
+
+// on click update background image
+$(document).ready(function(){
+	event.preventDefault();
+	$('#submit-btn').on('click',submit);
+});
